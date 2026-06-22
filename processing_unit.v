5d8f1082cc4f6f376bff4c8b30d57c9e247d9695
SV Aadhithya <aadhithyasv247@gmail.com>
2026-06-22 15:33:14 +0530

    Create processing_unit.v

---
+++ processing_unit.v
@@ -0,0 +1,32 @@
+module processing_unit(
+
+    input clk,
+    input sample_tick,
+
+    input [11:0] adc_data,
+
+    output reg [11:0] proc_data
+
+);
+
+reg [11:0] prev = 12'd0;
+
+reg [12:0] sum;
+
+always @(posedge clk)
+begin
+
+    if(sample_tick)
+    begin
+
+        sum = adc_data + prev;
+
+        proc_data <= sum >> 1;
+
+        prev <= adc_data;
+
+    end
+
+end
+
+endmodule
