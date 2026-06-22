4d2137ad4db2057feaadc1686698496ad42a8e94
SV Aadhithya <aadhithyasv247@gmail.com>
2026-06-22 15:35:41 +0530

    Create control_fsm.v

---
+++ control_fsm.v
@@ -0,0 +1,47 @@
+module control_fsm(
+
+    input clk,
+    input sample_tick,
+
+    output reg wr_en = 0,
+    output reg rd_en = 0
+
+);
+
+reg state = 0;
+
+always @(posedge clk)
+begin
+
+    if(sample_tick)
+    begin
+
+        case(state)
+
+            1'b0:
+            begin
+
+                wr_en <= 1;
+                rd_en <= 0;
+
+                state <= 1;
+
+            end
+
+            1'b1:
+            begin
+
+                wr_en <= 0;
+                rd_en <= 1;
+
+                state <= 0;
+
+            end
+
+        endcase
+
+    end
+
+end
+
+endmodule
