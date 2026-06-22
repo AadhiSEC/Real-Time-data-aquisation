bfc5c4889373f696b2dc92bde2cfee2d4d2340ad
SV Aadhithya <aadhithyasv247@gmail.com>
2026-06-22 15:34:39 +0530

    Create fifo.v

---
+++ fifo.v
@@ -0,0 +1,62 @@
+module fifo_buffer(
+
+    input clk,
+    input sample_tick,
+
+    input wr_en,
+    input rd_en,
+
+    input [11:0] din,
+
+    output reg [11:0] dout,
+
+    output full,
+    output empty
+
+);
+
+reg [11:0] mem [0:15];
+
+reg [3:0] w_ptr = 0;
+reg [3:0] r_ptr = 0;
+
+reg [4:0] count = 0;
+
+assign full  = (count == 16);
+
+assign empty = (count == 0);
+
+always @(posedge clk)
+begin
+
+    if(sample_tick)
+    begin
+
+        if(wr_en && !full)
+        begin
+
+            mem[w_ptr] <= din;
+
+            w_ptr <= w_ptr + 1;
+
+            count <= count + 1;
+
+        end
+
+        if(rd_en && !empty)
+        begin
+
+            dout <= mem[r_ptr];
+
+            r_ptr <= r_ptr + 1;
+
+            count <= count - 1;
+
+        end
+
+    end
+
+end
+
+endmodule
+
