262f4140672e6c2bf93b357d8651af0ad3968fee
SV Aadhithya <aadhithyasv247@gmail.com>
2026-06-22 15:31:49 +0530

    Create adc_dac_controller.v

---
+++ adc_dac_controller.v
@@ -0,0 +1,23 @@
+module adc_controller(
+
+    input clk,
+    input sample_tick,
+
+    input [11:0] mux_out,
+
+    output reg [11:0] adc_data
+
+);
+
+always @(posedge clk)
+begin
+
+    if(sample_tick)
+
+        adc_data <= mux_out;
+
+end
+
+endmodule
+
+
